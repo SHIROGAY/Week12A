492045fac55630b17ff66f91a8f2b2a3770c557a
SHIROGAY <awreeves@mymail.mines.edu>
2019-11-21 17:21:16 -0700

    try three conde 2

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Week12A
 This the week 12A assignment
+def PrintOutput(a):
+	print("OUTPUT",a)
--- week12.py
+++ week12.py
@@ -1,2 +1,12 @@
+#Aiden Reeves
+#CSCI 102- Section B
+#Week 12A
 def PrintOutput(a):
-    print("OUTPUT",a)
+    print("OUTPUT",a) #This is the first function that just makes it print output
+def LoadFile(b):
+    b=input("Enter file name: ")
+    with open(b,'r')as ins:
+        array=[] #this is to read a file line by line
+        for line in ins:
+            array.append(line)
+    PrintOutput(array)
